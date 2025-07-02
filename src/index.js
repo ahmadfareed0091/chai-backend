885f66c8d6df82301cfc08fe7f8ad33df4573664
ahmadfareed0091 <ahmadfareed18400@gmail.com>
2025-07-02 04:38:32 -0700

    util code discussion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 });
 
 
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("ERROR: ", err);  
+})
 
 
 
@@ -30,7 +38,7 @@ dotenv.config({
 
 /*
 
-import express from "express";
+import express from "express"; 
 const app = express();
 
 // iffe 
