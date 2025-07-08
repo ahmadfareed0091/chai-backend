85c4e26dedb3ccc2d68e7198343c4b60cd966c0e
ahmadfareed0091 <ahmadfareed18400@gmail.com>
2025-07-08 13:35:38 -0700

    correct errors

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: "./.env"
 });
-
+  
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+  app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
